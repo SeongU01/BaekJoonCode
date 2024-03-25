4c120de5d61d0cd4ddbf48145fe8e5e03f739e9b
SeongU01 <blacknaco85@gmail.com>
2024-03-25 10:14:56 +0900

    add 1110

---
+++ Code/week-4/1110.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+	int count = 0;
+	int temp = num;
+
+	while (true)
+	{
+		int a = temp % 10;
+		int b = ((a + temp / 10))%10;
+		temp = (a * 10) + b;
+		count++;
+		if (temp == num)
+			break;
+	}
+
+	cout << count;
+}
