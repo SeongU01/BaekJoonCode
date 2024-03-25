516928365c868929b98728e978ac886a64164761
SeongU01 <blacknaco85@gmail.com>
2024-03-25 18:28:46 +0900

    add

---
+++ Code/week-4/13458.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N,I,B,C;
+	long long answer=0;
+	cin >> N;
+
+	int *arr=new int[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> I;
+		arr[i] = I;
+	}
+
+	cin >> B >> C;
+
+	for (int i = 0; i < N; i++)
+	{
+		int inspector, subInspector;
+		inspector = 1;
+		if (arr[i] > B)
+		{
+			if (((arr[i] - B) % C)!=0)
+				subInspector = ((arr[i] - B) / C) + 1;
+			else
+				subInspector = ((arr[i] - B) / C);
+		}
+		else
+			subInspector = 0;
+
+		answer += (inspector + subInspector);
+	}
+
+	cout << answer;
+	
+	return 0;
+}
