ddd87b7128ec316aa6bf8cf612ea84546c0ac338
SeongU01 <blacknaco85@gmail.com>
2024-03-25 19:58:59 +0900

    add

---
+++ Code/week-4/1225.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string A,B;
+	cin >> A>>B;
+
+	int answer=0;
+	
+	for (int i = 0; i < A.size(); i++)
+	{
+		for (int j = 0; j < B.size(); j++)
+		{
+			answer += (A[i] - '0') * (B[j] - '0');
+		}
+	}
+
+	cout << answer;
+}
---
+++ Code/week-4/2851.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int score[10];
+	int scoreDiff[10]{ 0, };
+	int answer = 0;
+	int Min=0;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> score[i];
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		answer += score[i];
+		if (100 - answer >= 0)
+			scoreDiff[i] = 100 - answer;
+		else
+		{
+			scoreDiff[i] = answer - 100;
+			Min = i ;
+			break;
+		}
+	}
+
+	if (answer <= 100&&Min==0)
+	{
+		cout << answer;
+	}
+	else
+	{
+		if (scoreDiff[Min - 1] < scoreDiff[Min])
+			cout << answer - score[Min];
+
+		else
+			cout << answer;
+	}
+
+	return 0;
+}
